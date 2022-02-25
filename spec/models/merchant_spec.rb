9442c449710830b55539ef4a6051ddbae8e4b948
Devin Pile <devinpile@gmail.com>
2022-02-24 23:46:26 -0700

    Add model test for top 5 customers

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -27,5 +27,43 @@ describe Merchant do
         expect(merchant.ordered_items_to_ship).to eq([invoice_item1, invoice_item2])
       end
     end
+
+    it '#top_five_customers' do
+      merchant1 = create(:merchant)
+      item = create(:item, merchant_id: merchant1.id)
+      # customer1, 6 succesful transactions and 1 failed
+      customer1 = create(:customer)
+      invoice1 = create(:invoice, customer_id: customer1.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_item1 = create(:invoice_item, item_id: item.id, invoice_id: invoice1.id, status: 2)
+      transactions_list1 = FactoryBot.create_list(:transaction, 6, invoice_id: invoice1.id, result: 0)
+      failed1 = create(:transaction, invoice_id: invoice1.id, result: 1)
+      # customer2 5 succesful transactions
+      customer2 = create(:customer)
+      invoice2 = create(:invoice, customer_id: customer2.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_item2 = create(:invoice_item, item_id: item.id, invoice_id: invoice2.id, status: 2)
+      transactions_list2 = FactoryBot.create_list(:transaction, 5, invoice_id: invoice2.id, result: 0)
+      #customer3 4 succesful
+      customer3 = create(:customer)
+      invoice3 = create(:invoice, customer_id: customer3.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_item3 = create(:invoice_item, item_id: item.id, invoice_id: invoice3.id, status: 2)
+      transactions_list3 = FactoryBot.create_list(:transaction, 4, invoice_id: invoice3.id, result: 0)
+      #customer4 3 succesful
+      customer4 = create(:customer)
+      invoice4 = create(:invoice, customer_id: customer4.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_item4 = create(:invoice_item, item_id: item.id, invoice_id: invoice4.id, status: 2)
+      transactions_list4 = FactoryBot.create_list(:transaction, 3, invoice_id: invoice4.id, result: 0)
+      #customer5 2 succesful
+      customer5 = create(:customer)
+      invoice5 = create(:invoice, customer_id: customer5.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_item5 = create(:invoice_item, item_id: item.id, invoice_id: invoice5.id, status: 2)
+      transactions_list5 = FactoryBot.create_list(:transaction, 2, invoice_id: invoice5.id, result: 0)
+      #customer6 1 succesful
+      customer6 = create(:customer)
+      invoice6 = create(:invoice, customer_id: customer6.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_item6 = create(:invoice_item, item_id: item.id, invoice_id: invoice6.id, status: 2)
+      transactions_list6 = FactoryBot.create_list(:transaction, 1, invoice_id: invoice6.id, result: 0)
+
+      expect(merchant1.top_five_customers).to eq([customer1, customer2, customer3, customer4, customer5])
+    end
   end
 end
