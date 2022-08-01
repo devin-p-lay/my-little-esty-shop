612b92d08c31f56751ce0237981907caaf83b3b9
Devin Pile <devinpile@gmail.com>
2022-08-01 13:20:19 -0600

    Update project with refactor on merchant spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -42,22 +42,22 @@ describe Merchant do
       invoice2 = create(:invoice, customer_id: customer2.id, created_at: "2012-03-25 09:54:09 UTC")
       invoice_item2 = create(:invoice_item, item_id: item.id, invoice_id: invoice2.id, status: 2)
       transactions_list2 = FactoryBot.create_list(:transaction, 5, invoice_id: invoice2.id, result: 0)
-      #customer3 4 succesful
+      #customer3 4 succesful transactions
       customer3 = create(:customer)
       invoice3 = create(:invoice, customer_id: customer3.id, created_at: "2012-03-25 09:54:09 UTC")
       invoice_item3 = create(:invoice_item, item_id: item.id, invoice_id: invoice3.id, status: 2)
       transactions_list3 = FactoryBot.create_list(:transaction, 4, invoice_id: invoice3.id, result: 0)
-      #customer4 3 succesful
+      #customer4 3 succesful transactions
       customer4 = create(:customer)
       invoice4 = create(:invoice, customer_id: customer4.id, created_at: "2012-03-25 09:54:09 UTC")
       invoice_item4 = create(:invoice_item, item_id: item.id, invoice_id: invoice4.id, status: 2)
       transactions_list4 = FactoryBot.create_list(:transaction, 3, invoice_id: invoice4.id, result: 0)
-      #customer5 2 succesful
+      #customer5 2 succesful transactions
       customer5 = create(:customer)
       invoice5 = create(:invoice, customer_id: customer5.id, created_at: "2012-03-25 09:54:09 UTC")
       invoice_item5 = create(:invoice_item, item_id: item.id, invoice_id: invoice5.id, status: 2)
       transactions_list5 = FactoryBot.create_list(:transaction, 2, invoice_id: invoice5.id, result: 0)
-      #customer6 1 succesful
+      #customer6 1 succesful transactions
       customer6 = create(:customer)
       invoice6 = create(:invoice, customer_id: customer6.id, created_at: "2012-03-25 09:54:09 UTC")
       invoice_item6 = create(:invoice_item, item_id: item.id, invoice_id: invoice6.id, status: 2)
